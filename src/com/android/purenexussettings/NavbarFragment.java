a1183f113e5aae96bb3b89f13c8ea93812f11e95
Evisceration <eviscerationls@gmail.com>
2014-11-22 12:04:23 +0000

    (2/2) Settings: allow to configure the long press back kill delay
    PureNexus Edits:
    *forward ported to marshmallow
    *modified for our seekbar
    *allow to set in intervals of 250ms up to 3000ms

--- src/com/android/purenexussettings/NavbarFragment.java
+++ src/com/android/purenexussettings/NavbarFragment.java
@@ -15,24 +15,31 @@
  */
 package com.android.purenexussettings;
 
+import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 
-public class NavbarFragment extends PreferenceFragment {
+import com.android.purenexussettings.preferences.SeekBarPreference;
+
+public class NavbarFragment extends PreferenceFragment
+            implements OnPreferenceChangeListener  {
 
     private static final String CATEGORY_NAVBAR = "navigation_bar";
 
     private static final String NAVDIMEN = "navbar_dimen_frag";
     private static final String NAVBUTTON = "navbar_button_frag";
+    private static final String LONG_PRESS_KILL_DELAY = "long_press_kill_delay";
 
     private Preference mNavDimen;
     private Preference mNavButton;
-	
+    private SeekBarPreference mLongpressKillDelay;
+
     public NavbarFragment(){}
 
     @Override
@@ -41,12 +48,19 @@ public class NavbarFragment extends PreferenceFragment {
         addPreferencesFromResource(R.xml.navbar_fragment);
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
 
         final PreferenceCategory navbarCategory =
                 (PreferenceCategory) prefScreen.findPreference(CATEGORY_NAVBAR);
 
         mNavDimen = (Preference)findPreference(NAVDIMEN);
         mNavButton = (Preference)findPreference(NAVBUTTON);
+
+        mLongpressKillDelay = (SeekBarPreference) findPreference(LONG_PRESS_KILL_DELAY);
+        int killconf = Settings.System.getInt(resolver,
+                Settings.System.LONG_PRESS_KILL_DELAY, 1000);
+        mLongpressKillDelay.setValue(killconf);
+        mLongpressKillDelay.setOnPreferenceChangeListener(this);
 
         // Enable or disable NavbarImeSwitcher based on boolean: config_show_cmIMESwitcher
         boolean showCmImeSwitcher = getResources().getBoolean(
@@ -57,6 +71,18 @@ public class NavbarFragment extends PreferenceFragment {
                 navbarCategory.removePreference(pref);
             }
         }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mLongpressKillDelay) {
+            int killconf = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.LONG_PRESS_KILL_DELAY, killconf);
+            return true;
+        }
+        return false;
     }
 
     @Override
