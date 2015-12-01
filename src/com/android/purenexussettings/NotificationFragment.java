355b1c6c81c9eebcdafe2425461afa1e7da4a758
Muhammed Nazim <mnm9994u@gmail.com>
2015-12-01 06:15:03 -0500

    Forward Port: Add Camera sound toggle [2/3]
    PureNexus edits:
    * killed the dialog popup i come from touchwiz where we kill all the popups lol

--- src/com/android/purenexussettings/NotificationFragment.java
+++ src/com/android/purenexussettings/NotificationFragment.java
@@ -17,24 +17,42 @@
 package com.android.purenexussettings;
 
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
-import android.support.annotation.NonNull;
+import android.preference.SwitchPreference;
 
-public class NotificationFragment extends PreferenceFragment {
+public class NotificationFragment extends PreferenceFragment implements
+         OnPreferenceChangeListener {
+
     public NotificationFragment(){}
+
+    private static final String KEY_CAMERA_SOUNDS = "camera_sounds";
+    private static final String PROP_CAMERA_SOUND = "persist.sys.camera-sound";
+
+    private SwitchPreference mCameraSounds;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.notification_fragment);
+
+        mCameraSounds = (SwitchPreference) findPreference(KEY_CAMERA_SOUNDS);
+        mCameraSounds.setChecked(SystemProperties.getBoolean(PROP_CAMERA_SOUND, true));
+        mCameraSounds.setOnPreferenceChangeListener(this);
     }
 
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen prefScreen, @NonNull Preference pref) {
-        return false;
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        if (KEY_CAMERA_SOUNDS.equals(key)) {
+           if ((Boolean) objValue) {
+               SystemProperties.set(PROP_CAMERA_SOUND, "1");
+           } else {
+               SystemProperties.set(PROP_CAMERA_SOUND, "0");
+           }
+        }
+        return true;
     }
 }
