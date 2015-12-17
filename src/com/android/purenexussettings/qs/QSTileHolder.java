ellienieuwdorp/android_packages_apps_Settings

Settings : Add adb over network tile
Change-Id: I6c30ff1d40dddc01a52e450b860351fca4123fe4

src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -90,6 +90,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_hotspot_on";
                 stringId = R.string.hotspot;
                 break;
+            case QSConstants.TILE_ADB_NETWORK:
+                resourceName = "ic_qs_network_adb_on";
+                stringId = R.string.adb_over_network;
+                break;
             default:
                 return null;
         }

tornado76/android_packages_apps_PureNexusSettings

src/com/android/purenexussettings/qs/QSTileHolder.java
@@ -94,6 +94,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_hotspot_on";
                 stringId = R.string.hotspot;
                 break;
+            case QSConstants.TILE_ADB_NETWORK:
+                resourceName = "ic_qs_network_adb_on";
+                stringId = R.string.qs_tile_adb_over_network;
+                break;
             default:
                 return null;
         }