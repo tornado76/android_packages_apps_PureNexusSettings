LineageOS/android_packages_apps_Settings

QS: Add Ambient Display Tile (2/2)
Change-Id: I332ac0b9d69d7c3c8029511fd97ae81277ebe598

src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -153,6 +153,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_heads_up_on";
                 stringId = R.string.qs_tile_heads_up;
                 break;
+            case QSConstants.TILE_AMBIENT_DISPLAY:
+                resourceName = "ic_qs_ambientdisplay_on";
+                stringId = R.string.qs_ambient_display;
+                break;
             default:
                 return null;
         }

tornado76/android_packages_apps_PureNexusSettings

src/com/android/purenexussettings/qs/QSTileHolder.java
@@ -134,6 +134,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_volume_panel";
                 stringId = R.string.qs_tile_volume_panel;
                 break;
+            case QSConstants.TILE_AMBIENT_DISPLAY:
+                resourceName = "ic_qs_ambientdisplay_on";
+                stringId = R.string.qs_tile_ambient_display;
+                break;
             default:
                 return null;
         }